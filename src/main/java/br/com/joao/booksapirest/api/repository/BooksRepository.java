f7b94b77b8e218c444e6c02bf4c1ff8ffe612d99
João Oliveira <xliveirvwrld@gmail.com>
2025-07-25 17:48:43 -0300

    feat: update dependencies and add db settings

--- src/main/java/br/com/joao/booksapirest/api/repository/BooksRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.joao.booksapirest.api.repository;
-
-import br.com.joao.booksapirest.api.domain.book.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BooksRepository extends JpaRepository<Book, Long> {
-}
